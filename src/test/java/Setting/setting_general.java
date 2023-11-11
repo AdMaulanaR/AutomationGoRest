98aa2a34c1d52826f9795e0206e3946781fc90cf
ADMaulana <forfunader9@gmail.com>
2023-11-11 21:03:02 +0800

    Merge remote-tracking branch 'origin/main'

--- src/test/java/Setting/setting_general.java
+++ src/test/java/Setting/setting_general.java
@@ -4,8 +4,6 @@ public class setting_general {
     public String token = "1dfdb361992d13c0b09a2f558f49de5b0cbe97f08827d4d2867322afdd83e82d";
 
     //public static RequestSpecification request
-    public static void newId(){
-    }
 
 
 }
